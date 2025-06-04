d954984e663e3355885d6eab74f44dca80a85a52
erneto13 <erneto.amaral13@gmail.com>
2025-06-04 13:34:30 -0700

    chochua gei

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://ntic-checadorbackend-production-6448.up.railway.app/api/v1'
+    apiUrl: 'https://ntic-checadorbackend-production-6448.up.railway.app/api/v1'
 };
